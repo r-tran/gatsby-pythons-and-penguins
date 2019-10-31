daac582b63f2842e171dd857bbc97a475666c918
Ray Tran <r8tran@gmail.com>
2019-10-30 22:28:29 -0700

    add in examples

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -4,4 +4,8 @@ import "typeface-merriweather"
 import installFontAwesome from './src/api/installFontAwesome'
 import "./src/styles/global.css"
 
+require("prismjs/themes/prism-solarizedlight.css")
+require("prismjs/plugins/line-numbers/prism-line-numbers.css")
+require("prismjs/plugins/command-line/prism-command-line.css")
+
 installFontAwesome();
