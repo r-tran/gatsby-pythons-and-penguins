4b9f2aacd21d26b8f19c8cda88a419bbb7cd41ec
Ray Tran <r8tran@gmail.com>
2019-11-03 21:44:16 -0800

    add in aria labels

--- src/components/social.js
+++ src/components/social.js
@@ -2,9 +2,10 @@ import React from "react"
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { useStaticQuery, graphql } from "gatsby"
 
-const Icon = ({ href, icon }) => (
+const Icon = ({ href, icon, ariaLabel, }) => (
   <a
     href={href}
+    aria-label={ariaLabel}
   >
     <span className="fa-layers fa-fw fa-2x">
       <FontAwesomeIcon icon={icon} />
@@ -35,20 +36,27 @@ const SocialLinks = () => {
         <Icon
           href={`https://twitter.com/${social.twitter}`}
           icon={['fab', 'twitter']}
+          ariaLabel={"The twitter account link"}
         />
         <Icon
           href={`https://github.com/${social.github}`}
           icon={['fab', 'github']}
+          ariaLabel={"The github account link"}
         />
         <Icon
           href={`https://dev.to/${social.dev}`}
           icon={['fab', 'dev']}
+          ariaLabel={"The dev account link"}
         />
         <Icon
           href={`https://www.linkedin.com/in/${social.linkedin}/`}
           icon={['fab', 'linkedin']}
+          ariaLabel={"The linkedin account link"}
         />
-        <Icon href={`mailto:${social.email}`} icon={['fas', 'envelope']} />
+        <Icon 
+          href={`mailto:${social.email}`} 
+          icon={['fas', 'envelope']} 
+          ariaLabel={"The email address to contact"}/>
       </div>
   )
 }
