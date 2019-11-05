09f0c80d82f1ebdb47a853fde77f204ef05d92eb
Ray Tran <r8tran@gmail.com>
2019-11-04 21:30:10 -0800

    add in tracking id

--- gatsby-config.js
+++ gatsby-config.js
@@ -56,12 +56,6 @@ module.exports = {
     },
     `gatsby-transformer-sharp`,
     `gatsby-plugin-sharp`,
-    {
-      resolve: `gatsby-plugin-google-analytics`,
-      options: {
-        //trackingId: `ADD YOUR TRACKING ID HERE`,
-      },
-    },
     `gatsby-plugin-feed`,
     {
       resolve: `gatsby-plugin-manifest`,
@@ -82,6 +76,12 @@ module.exports = {
       resolve: `gatsby-plugin-typography`,
       options: {
         pathToConfigModule: `src/utils/typography`,
+      },
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        trackingId: "UA-151534722-1",
       },
     },
   ],
